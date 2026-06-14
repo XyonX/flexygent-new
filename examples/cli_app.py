0a33c6553f147ea9c8d02bee0116c23438b486d7
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2026-06-14 21:26:09 +0530

    Implement CLI application for conversation management and interaction

---
+++ examples/cli_app.py
@@ -0,0 +1,68 @@
+# cli_app.py
+
+from flexygent.types import Conversation,Message,Role,AgentConfig
+from flexygent.types import Agent
+from flexygent.skills import skill_registry,flex_skills
+from flexygent.tools import tool_registry, get_tools
+from flexygent.agent import agent_loop
+from flexygent.client import client
+from flexygent.memory.file_store import FileStore
+
+def app():
+    # use the framrwork and make the cli app
+    
+    config  = AgentConfig(model="deepseek-v4-flash")
+
+    flex = Agent(name="flex",config=config)
+    flex.apply_skills(flex_skills,skill_registry)
+
+    system_message = flex.get_system_message()
+
+    conv = Conversation()
+    conv.add_message(system_message)
+
+        # # make tools payload 
+    tool_filter= flex.get_tool_filter(skill_registry)
+    tools= get_tools(tool_registry,tool_filter)
+
+
+    memory =FileStore()
+
+
+    # saved_conversation_files = get_saved_files()
+    saved_conversation_files = memory.list_saved()
+
+    if(len(saved_conversation_files) !=0):
+        print("Saved file detected , would you like to load the latest one ? ")
+        input_value = input()
+        no_val = ["no","n","nahi"]
+        if input_value in no_val:
+            pass
+        else:
+            # conv=load_conversation(saved_conversation_files[0])
+            conv=memory.load(saved_conversation_files[0])
+
+            
+    
+    while 1:
+
+        # take message
+        input_message = input("Enter message : ")
+
+        print("\n")
+
+        if input_message == "exit":
+            return conv
+        
+        output_message = agent_loop(conv,input_message,tools,tool_registry,client,flex.config)
+
+        # print response 
+        print("assistant: ",output_message)
+        print("\n")
+    
+    return conv
+
+
+
+if __name__ == "__main__":
+    app()
